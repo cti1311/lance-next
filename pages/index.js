ff7c28b3cb5ef394108fbf907220d5208625eb34
kshitij tomar <31240061+cti1311@users.noreply.github.com>
2024-07-19 00:08:38 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
   const [data, setData] = useState([]);
   const [time, setTime] = useState([]);
   useEffect(async () => {
-    let res = await fetch("https://api.0xcti.in/api/v1/statu");
+    let res = await fetch("https://api.0xcti.in/api/v1/status");
     res = await res.json();
     setTime(res.timeStamp);
     setData(res.data);
